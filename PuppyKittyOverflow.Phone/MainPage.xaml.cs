2ede46992303502754fbada0d758c32c3fb664fa
James Montemagno <james.montemagno@gmail.com>
2013-11-17 21:15:18 -0800

    Initial Commit

---
+++ PuppyKittyOverflow.Phone/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Phone.Controls;
+using PuppyKittyOverflow.Portable;
+
+namespace PuppyKittyOverflow.Phone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+       
+        private async void ButtonCat_OnClick(object sender, RoutedEventArgs e)
+        {
+            MarkedUp.AnalyticClient.SessionEvent("Cat");
+            LoadImage(true);
+        }
+
+        private async void ButtonDog_OnClick(object sender, RoutedEventArgs e)
+        {
+            MarkedUp.AnalyticClient.SessionEvent("Dog");
+           LoadImage(false);
+        }
+
+        private string image;
+        private async void LoadImage(bool cat)
+        {
+            LoadingProgressBar.Visibility = Visibility.Visible;
+            CatDogImage.Visibility = Visibility.Collapsed;
+            ButtonCat.IsEnabled = false;
+            ButtonDog.IsEnabled = false;
+            ButtonSetLockScreen.IsEnabled = false;
+            image =
+                await OverflowHelper.GetPictureAsync(cat ? OverflowHelper.Animal.Cat : OverflowHelper.Animal.Dog);
+
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(image))
+                {
+                    image = cat ? @"Assets\Cat.png" : @"Assets\Dog.png";
+
+                    CatDogImage.Source = new BitmapImage(new Uri(image, UriKind.Relative));
+                }
+                else
+                {
+                    CatDogImage.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
+                }
+            }
+            catch (Exception)
+            {
+                image = cat ? @"Assets\Cat.png" : @"Assets\Dog.png";
+
+                CatDogImage.Source = new BitmapImage(new Uri(image, UriKind.Relative));
+            }
+
+
+            LoadingProgressBar.Visibility = Visibility.Collapsed;
+            CatDogImage.Visibility = Visibility.Visible;
+            ButtonCat.IsEnabled = true;
+            ButtonDog.IsEnabled = true;
+            ButtonSetLockScreen.IsEnabled = true;
+        }
+
+        private void ButtonSetLockScreen_OnClick(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ PuppyKittyOverflow.Portable/OverflowHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PuppyKittyOverflow.Portable
+{
+
+    
+
+    public static class OverflowHelper
+    {
+        public enum Animal
+        {
+            Cat,
+            Dog
+        }
+
+        private const string CatUrl = "http://catoverflow.com/api/query?limit=1&order=random";
+        private const string DogUrl = "http://dogoverflow.com/api/query?limit=1&order=random";
+        public async static Task<String> GetPictureAsync(Animal animal)
+        {
+            try
+            {
+                var url = animal == Animal.Cat ? CatUrl : DogUrl;
+
+                using (var client = new HttpClient())
+                {
+                    return await client.GetStringAsync(url);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+
+            }
+            
+        }
+    }
+}
